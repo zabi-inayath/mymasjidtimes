c8790d98b9a0e96e399184e186f5466339723cbe
Zabiullah S <zabi2201@hotmail.com>
2025-07-11 11:12:33 +0530

    Bottom bugs fixed and optimized UI

--- MMT-frontend/src/App.jsx
+++ MMT-frontend/src/App.jsx
@@ -2,37 +2,48 @@ import React from 'react';
 import './App.css';
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
 import MasjidLoginPage from './components/MasjidComponents/MasjidLoginPage';
 import MasjidSignupPage from './components/MasjidComponents/MasjidSignupPage';
 import MasjidDashboard from './components/MasjidComponents/MasjidDashboard';
-import AppHome from './components/UserComponents/AppHome';
 import PrayerTimesPage from './components/UserComponents/PrayerTimesPage';
+
+import AppHome from './components/UserComponents/AppHome';
+import HomePage from './components/UserComponents/HomePage';
+import MasjidListPage from './components/UserComponents/MasjidListPage';
+import FavouritePage from './components/UserComponents/FavouritePage';
+import LearningsPage from './components/UserComponents/LearningsPage';
+import AboutPage from './components/UserComponents/AboutPage';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          {/* Redirect root to login */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-
-          {/* Masjid route */}
+          {/* Masjid routes */}
           <Route path="/masjid/login" element={<MasjidLoginPage />} />
           <Route path="/masjid/signup" element={<MasjidSignupPage />} />
           <Route path="/masjid/dashboard" element={<MasjidDashboard />} />
 
           {/* User routes */}
-          <Route path="/home" element={<AppHome />} />
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<AppHome><HomePage /></AppHome>} />
+          <Route path="/masjids" element={<AppHome><MasjidListPage /></AppHome>} />
+          <Route path="/favorite" element={<AppHome><FavouritePage /></AppHome>} />
+          <Route path="/learn" element={<AppHome><LearningsPage /></AppHome>} />
+          <Route path="/about" element={<AppHome><AboutPage /></AppHome>} />
+
           <Route path="/masjid-timing/:id" element={<PrayerTimesPage />} />
-          {/* <Route path="/masjid/settings" element={<Settings />} /> */}
 
           {/* 404 - Not found route */}
-          <Route path="*" element={<div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <div className="text-center">
-              <h1 className="text-4xl font-bold text-gray-800 mb-4">404</h1>
-              <p className="text-gray-600">Page not found</p>
+          <Route path="*" element={
+            <div className="min-h-screen flex items-center justify-center bg-gray-100">
+              <div className="text-center">
+                <h1 className="text-4xl font-bold text-gray-800 mb-4">404</h1>
+                <p className="text-gray-600">Page not found</p>
+              </div>
             </div>
-          </div>} />
+          } />
         </Routes>
         <Toaster position="top-center" reverseOrder={true} />
       </div>
--- MMT-frontend/src/components/UserComponents/AppHome.jsx
+++ MMT-frontend/src/components/UserComponents/AppHome.jsx
@@ -1,35 +1,13 @@
-import React, { useState } from 'react';
-import HomePage from './HomePage';
-import MasjidListPage from './MasjidListPage';
-import FavouritePage from './FavouritePage';
-import LearningsPage from './LearningsPage';
-import AboutPage from './AboutPage';
+import React from 'react';
 import BottomNav from './BottomNav';
 
-const AppHome = () => {
-    const [currentPage, setCurrentPage] = useState('home');
-
-    const renderPage = () => {
-        switch (currentPage) {
-            case 'home':
-                return <HomePage />;
-            case 'masjid':
-                return <MasjidListPage />;
-            case 'favourite':
-                return <FavouritePage />;
-            case 'learnings':
-                return <LearningsPage />;
-            case 'about':
-                return <AboutPage />;
-            default:
-                return <HomePage />;
-        }
-    };
-
+const AppHome = ({ children }) => {
     return (
-        <div className="max-w-md mx-auto bg-[#fef9ef] min-h-screen">
-            {renderPage()}
-            <BottomNav currentPage={currentPage} setCurrentPage={setCurrentPage} />
+        <div className="max-w-md mx-auto bg-[#fef9ef] min-h-screen flex flex-col">
+            <div className="flex-grow">
+                {children}
+            </div>
+            <BottomNav />
         </div>
     );
 };
