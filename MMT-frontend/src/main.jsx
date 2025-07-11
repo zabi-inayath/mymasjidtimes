f3153c0237a4917a1f63354f1a48d4b66b24b6ff
Zabiullah S <zabi2201@hotmail.com>
2025-07-11 18:51:39 +0530

    bug fixed for PWA

--- MMT-frontend/src/main.jsx
+++ MMT-frontend/src/main.jsx
@@ -7,3 +7,17 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+// Register service worker manually:
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('✅ Service Worker registered:', registration)
+      })
+      .catch((error) => {
+        console.log('❌ Service Worker registration failed:', error)
+      })
+  })
+}
